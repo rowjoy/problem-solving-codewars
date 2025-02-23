f4f7e60a352a79d61d6c96475a63f73131100ac5
rowjoy <rowjoy202@gmail.com>
2025-02-23 17:51:01 +0600

    Two problem solved

--- dart/Human Readable Time.dart
+++ dart/Human Readable Time.dart
@@ -5,7 +5,6 @@ void main(){
 }
 
 String humanReadable(int seconds) {
-   
    int hours = seconds ~/ 3600;  //since 3600 seconds = 1 hour
    int minutes = (seconds % 3600) ~/ 60; // Uses ~/ for integer division.
    int secondsd = seconds % 60; //Uses % (modulo) to get the remainder.
@@ -13,14 +12,5 @@ String humanReadable(int seconds) {
    String h = hours < 10 ? "0$hours" : "$hours";
    String m = minutes < 10 ? "0$minutes" : "$minutes";
    String s = secondsd < 10 ? "0$secondsd" : "$secondsd";
-
-   print("$h:$m:$s");
-  
-
-
-
-
-
-  // type here !
-  return '';
+  return '$h:$m:$s';
 }
---
+++ dart/IP Validation.dart
@@ -0,0 +1,15 @@
+void main(){
+  print("hello");
+  isValidIP('abc.def.ghi.jkl');
+}
+
+
+bool isValidIP(String input){
+  final RegExp ipv4Regex = RegExp(
+    r'^(25[0-5]|2[0-4][0-9]|1[0-9]{2}|[1-9]?[0-9])'
+    r'(\.(25[0-5]|2[0-4][0-9]|1[0-9]{2}|[1-9]?[0-9])){3}$',
+  );
+  return ipv4Regex.hasMatch(input);
+}
+//https://www.codewars.com/kata/54d496788776e49e6b00052f/train/dart
+//224,369
